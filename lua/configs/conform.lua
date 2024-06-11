1cfe4d64fa327f4a13c9b0587fb5906af52606e2
petar.marinov <petar.marinov@trading212.com>
2024-06-11 12:51:36 +0300

    Switched to biome

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,6 +1,10 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
+    typescript = { "biome" },
+    typescriptreact = { "biome" },
+    javascript = { "biome" },
+    javascriptreact = { "biome" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
@@ -8,7 +12,7 @@ local options = {
   -- format_on_save = {
   --   -- These options will be passed to conform.format()
   --   timeout_ms = 500,
-  --   lsp_fallback = true,
+  --   lsp_fallback = false,
   -- },
 }
 
